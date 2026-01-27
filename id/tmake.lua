0c3f78772f6cb2b0137a42cd7e42548a920976cc
yfdyzjt <yfdyzjt@gmail.com>
2026-01-28 01:07:50 +0800

    Add item classification script based on id

--- id/tmake.lua
+++ id/tmake.lua
@@ -8,95 +8,98 @@ local startFramePos = Point(4291, 1288)
 local partStep = Point(45, -45)
 local partCount = Point(8, 9)
 
-local worldName = "all_item_world_master.wld"
-local world = LoadWorld(worldName)
-print("Load world: " .. world.Name)
+local worldNames = {"all_item_world.wld", "all_item_world_classic.wld", "all_item_world_expert.wld", "all_item_world_master.wld"}
 
-local frameId : int = 1
-local chestId : int = 1
+for _, worldName in ipairs(worldNames) do
+    local world = LoadWorld(worldName)
+    print("Load world: " .. world.Name)
 
-print("Item frame items:")
+    local frameId : int = 1
+    local chestId : int = 1
 
-local minFrameX : int = startFramePos.X
-local maxFrameX : int = startFramePos.X + partStep.X * (partCount.X - 1)
-local stepFrameX : int = partStep.X
-local minFrameY : int = startFramePos.Y
-local maxFrameY : int = startFramePos.Y + partStep.Y * (partCount.Y - 1)
-local stepFrameY : int = partStep.Y
+    print("Item frame items:")
 
-for partY : int = minFrameY, maxFrameY, stepFrameY  do
-    for partX : int = minFrameX, maxFrameX, stepFrameX do
-        if frameId > maxId then break end
+    local minFrameX : int = startFramePos.X
+    local maxFrameX : int = startFramePos.X + partStep.X * (partCount.X - 1)
+    local stepFrameX : int = partStep.X
+    local minFrameY : int = startFramePos.Y
+    local maxFrameY : int = startFramePos.Y + partStep.Y * (partCount.Y - 1)
+    local stepFrameY : int = partStep.Y
 
-        local startId = frameId
+    for partY : int = minFrameY, maxFrameY, stepFrameY  do
+        for partX : int = minFrameX, maxFrameX, stepFrameX do
+            if frameId > maxId then break end
 
-        for offsetY : int = 0, 18, 2 do
-            for offsetX : int = 0, 18, 2 do
-                if frameId > maxId then break end
+            local startId = frameId
 
-                local x : int = partX + offsetX
-                local y : int = partY + offsetY
+            for offsetY : int = 0, 18, 2 do
+                for offsetX : int = 0, 18, 2 do
+                    if frameId > maxId then break end
 
-                local pos = Point(x, y)
+                    local x : int = partX + offsetX
+                    local y : int = partY + offsetY
 
-                local frame = Tool.GetTileEntity(world, pos)
+                    local pos = Point(x, y)
 
-                frame.Item.Type = frameId
-                frame.Item.StackSize = 1
+                    local frame = Tool.GetTileEntity(world, pos)
 
-                frameId = frameId + 1
-            end
-        end
+                    frame.Item.Type = frameId
+                    frame.Item.StackSize = 1
 
-        print(startId .. " ~ " .. frameId - 1)
-    end
-end
-
-print("Chest items:")
-
-local minChestX : int = startChestPos.X
-local maxChestX : int = startChestPos.X + partStep.X * (partCount.X - 1)
-local stepChestX : int = partStep.X
-local minChestY : int = startChestPos.Y
-local maxChestY : int = startChestPos.Y + partStep.Y * (partCount.Y - 1)
-local stepChestY : int = partStep.Y
-
-for partY : int = minChestY, maxChestY, stepChestY  do
-    for partX : int = minChestX, maxChestX, stepChestX do
-        if chestId > maxId then break end
-
-        local startId = chestId
-
-        for offsetY : int = 0, 6, 3 do
-            chestId = startId
-
-            for offsetX : int = 0, 4, 2 do
-                if chestId > maxId then break end
-
-                local x : int = partX + offsetX
-                local y : int = partY + offsetY
-
-                local pos = Point(x, y)
-                local chest = Tool.GetChest(world, pos)
-
-                local beginId = chestId
-
-                for i = 0, ChestProperty.MaxItems - 1, 1 do
-                    if chestId > maxId or chestId - startId >= 100 then break end
-                    
-                    chest.Item[i].StackSize = 9999
-                    chest.Item[i].Type = chestId
-
-                    chestId = chestId + 1
+                    frameId = frameId + 1
                 end
-
-                chest.Name = beginId .. " ~ " .. chestId - 1
             end
+
+            print(startId .. " ~ " .. frameId - 1)
         end
-
-        print(startId .. " ~ " .. chestId - 1)
     end
-end
 
-print("Save world: " .. world.Name)
-SaveWorld(world)
+    print("Chest items:")
+
+    local minChestX : int = startChestPos.X
+    local maxChestX : int = startChestPos.X + partStep.X * (partCount.X - 1)
+    local stepChestX : int = partStep.X
+    local minChestY : int = startChestPos.Y
+    local maxChestY : int = startChestPos.Y + partStep.Y * (partCount.Y - 1)
+    local stepChestY : int = partStep.Y
+
+    for partY : int = minChestY, maxChestY, stepChestY  do
+        for partX : int = minChestX, maxChestX, stepChestX do
+            if chestId > maxId then break end
+
+            local startId = chestId
+
+            for offsetY : int = 0, 6, 3 do
+                chestId = startId
+
+                for offsetX : int = 0, 4, 2 do
+                    if chestId > maxId then break end
+
+                    local x : int = partX + offsetX
+                    local y : int = partY + offsetY
+
+                    local pos = Point(x, y)
+                    local chest = Tool.GetChest(world, pos)
+
+                    local beginId = chestId
+
+                    for i = 0, ChestProperty.MaxItems - 1, 1 do
+                        if chestId > maxId or chestId - startId >= 100 then break end
+                        
+                        chest.Item[i].StackSize = 9999
+                        chest.Item[i].Type = chestId
+
+                        chestId = chestId + 1
+                    end
+
+                    chest.Name = beginId .. " ~ " .. chestId - 1
+                end
+            end
+
+            print(startId .. " ~ " .. chestId - 1)
+        end
+    end
+
+    print("Save world: " .. world.Name)
+    SaveWorld(world, worldName)
+end 
