a467ca2b45d3081d0fdeb5564d2522823b559350
yfdyzjt <yfdyzjt@gmail.com>
2026-01-28 23:21:01 +0800

    Add item classification write

--- class/patch.cs
+++ class/patch.cs
@@ -7,6 +7,8 @@ using Terraria.Localization;
 // ###################################################################################
 using Terraria.GameContent;
 using Terraria.ID;
+using System.Linq;
+using System.IO;
 // ###################################################################################
 namespace Terraria.Chat
 {
@@ -17,6 +19,15 @@ namespace Terraria.Chat
             // ###################################################################################
             if (message.Text == "/start")
             {
+                string weapons = "Weapons", tools = "Tools", armor = "Armor", accessories = "Accessories", placeables = "Placeables", consumables = "Consumables", others = "Others";
+                string melee = "Melee", magic = "Magic", ranged = "Ranged", summon = "Summon", whips = "Whips", throwing = "Throwing", sentry = "Sentry";
+                string pickaxes = "Pickaxes", axes = "Axes", hammers = "Hammers", drill = "Drill", chainsaw = "Chainsaw";
+                string head = "Head", body = "Body", legs = "Legs", vanity = "Vanity";
+                string aaccessories = "Accessories", wings = "Wings", hooks = "Hooks", pets = "Pets", lightPets = "LightPets", mounts = "Mounts", carts = "Carts";
+                string tiles = "Tiles", walls = "Walls", furniture = "Furniture", fishingCrate = "FishingCrate", relic = "Relic";
+                string ammo = "Ammo", potions = "Potins", food = "Food", bossSummons = "BossSummons", bossBag = "BossBag", bait = "Bait", questFish = "QuestFish", pickup = "Pickup";
+                string materials = "Materials", kites = "Kites", poles = "Poles", paint = "Paint", dyes = "Dyes", oothers = "Others";
+
                 var categories = new Dictionary<string, Dictionary<string, List<Item>>>();
 
                 bool[] spears = ItemID.Sets.Factory.CreateBoolSet(ItemID.Spear, ItemID.Trident, ItemID.Swordfish, ItemID.ThunderSpear, ItemID.TheRottedFork, ItemID.DarkLance, ItemID.CobaltNaginata, ItemID.PalladiumPike, ItemID.MythrilHalberd, ItemID.OrichalcumHalberd, ItemID.AdamantiteGlaive, ItemID.TitaniumTrident, ItemID.ObsidianSwordfish, ItemID.Gungnir, ItemID.MushroomSpear, ItemID.MonkStaffT1, ItemID.MonkStaffT2, ItemID.MonkStaffT3, ItemID.ChlorophytePartisan, ItemID.NorthPole);
@@ -24,7 +35,7 @@ namespace Terraria.Chat
                 List<int> sortingPriorityBossSpawnsExclusions = new List<int> { ItemID.LifeCrystal, ItemID.ManaCrystal, ItemID.CellPhone, ItemID.IceMirror, ItemID.MagicMirror, ItemID.LifeFruit, ItemID.TreasureMap, ItemID.Shellphone, ItemID.ShellphoneDummy, ItemID.ShellphoneHell, ItemID.ShellphoneOcean, ItemID.ShellphoneSpawn, ItemID.MagicConch, ItemID.DemonConch };
 
                 // 初始化
-                for (int id = 1; id < TextureAssets.Item.Length; id++)
+                for (int id = 1; id < ItemID.Count; id++)
                 {
                     if (!ItemID.Sets.Deprecated[id])
                     {
@@ -37,101 +48,101 @@ namespace Terraria.Chat
 
                         // 武器
                         if (item.melee && !isTool)
-                        { categories["Weapons"]["Melee"].Add(item); other = false; } // 近战
+                        { categories[weapons][melee].Add(item); other = false; } // 近战
                         if (item.magic)
-                        { categories["Weapons"]["Magic"].Add(item); other = false; } // 魔法
+                        { categories[weapons][magic].Add(item); other = false; } // 魔法
                         if (item.ranged && item.ammo == 0)
-                        { categories["Weapons"]["Ranged"].Add(item); other = false; } // 远程
+                        { categories[weapons][ranged].Add(item); other = false; } // 远程
                         if (item.summon && !meleeNoSpeed && !item.sentry)
-                        { categories["Weapons"]["Summon"].Add(item); other = false; } // 召唤
+                        { categories[weapons][summon].Add(item); other = false; } // 召唤
                         if (meleeNoSpeed && !item.sentry)
-                        { categories["Weapons"]["Whips"].Add(item); other = false; } // 鞭子
+                        { categories[weapons][whips].Add(item); other = false; } // 鞭子
                         if (item.noMelee && item.ranged && item.consumable)
-                        { categories["Weapons"]["Throwing"].Add(item); other = false; } // 投掷 
+                        { categories[weapons][throwing].Add(item); other = false; } // 投掷 
                         if (item.summon && item.sentry)
-                        { categories["Weapons"]["Sentry"].Add(item); other = false; } // 炮塔
+                        { categories[weapons][sentry].Add(item); other = false; } // 炮塔
 
                         // 工具
                         if (item.pick > 0 && !ItemID.Sets.IsDrill[item.type])
-                        { categories["Tools"]["Pickaxes"].Add(item); other = false; } // 镐子
+                        { categories[tools][pickaxes].Add(item); other = false; } // 镐子
                         if (item.axe > 0 && !ItemID.Sets.IsChainsaw[item.type])
-                        { categories["Tools"]["Axes"].Add(item); other = false; } // 斧头
+                        { categories[tools][axes].Add(item); other = false; } // 斧头
                         if (item.hammer > 0)
-                        { categories["Tools"]["Hammers"].Add(item); other = false; } // 锤子
+                        { categories[tools][hammers].Add(item); other = false; } // 锤子
                         if (ItemID.Sets.IsDrill[item.type])
-                        { categories["Tools"]["Drill"].Add(item); other = false; } // 钻头
+                        { categories[tools][drill].Add(item); other = false; } // 钻头
                         if (ItemID.Sets.IsChainsaw[item.type])
-                        { categories["Tools"]["Chainsaw"].Add(item); other = false; } // 链锯
+                        { categories[tools][chainsaw].Add(item); other = false; } // 链锯
 
                         // 盔甲
                         if (item.headSlot != -1 && !item.vanity)
-                        { categories["Armor"]["Head"].Add(item); other = false; } // 头部
+                        { categories[armor][head].Add(item); other = false; } // 头部
                         if (item.bodySlot != -1 && !item.vanity)
-                        { categories["Armor"]["Body"].Add(item); other = false; } // 身体
+                        { categories[armor][body].Add(item); other = false; } // 身体
                         if (item.legSlot != -1 && !item.vanity)
-                        { categories["Armor"]["Legs"].Add(item); other = false; } // 腿部
+                        { categories[armor][legs].Add(item); other = false; } // 腿部
                         if (item.vanity)
-                        { categories["Armor"]["Vanity"].Add(item); other = false; } // 时装
+                        { categories[armor][vanity].Add(item); other = false; } // 时装
 
                         // 饰品
                         if (item.accessory && item.wingSlot <= 0)
-                        { categories["Accessories"]["Accessories"].Add(item); other = false; } // 饰品
+                        { categories[accessories][aaccessories].Add(item); other = false; } // 饰品
                         if (item.accessory && item.wingSlot > 0)
-                        { categories["Accessories"]["Wings"].Add(item); other = false; } // 翅膀
+                        { categories[accessories][wings].Add(item); other = false; } // 翅膀
                         if (Main.projHook[item.shoot])
-                        { categories["Accessories"]["Hooks"].Add(item); other = false; } // 钩爪
+                        { categories[accessories][hooks].Add(item); other = false; } // 钩爪
                         if (Main.vanityPet[item.buffType])
-                        { categories["Accessories"]["Pets"].Add(item); other = false; } // 宠物
+                        { categories[accessories][pets].Add(item); other = false; } // 宠物
                         if (Main.lightPet[item.buffType])
-                        { categories["Accessories"]["LightPets"].Add(item); other = false; } // 照明宠物
+                        { categories[accessories][lightPets].Add(item); other = false; } // 照明宠物
                         if (item.mountType != -1 && !MountID.Sets.Cart[item.mountType])
-                        { categories["Accessories"]["Mounts"].Add(item); other = false; } // 坐骑
+                        { categories[accessories][mounts].Add(item); other = false; } // 坐骑
                         if (item.mountType != -1 && MountID.Sets.Cart[item.mountType])
-                        { categories["Accessories"]["Carts"].Add(item); other = false; } // 矿车
+                        { categories[accessories][carts].Add(item); other = false; } // 矿车
 
                         // 放置物
                         if (item.createTile != -1 && !Main.tileFrameImportant[item.createTile])
-                        { categories["Placeables"]["Tiles"].Add(item); other = false; } // 物块
+                        { categories[placeables][tiles].Add(item); other = false; } // 物块
                         if (item.createWall != -1)
-                        { categories["Placeables"]["Walls"].Add(item); other = false; } // 墙壁
+                        { categories[placeables][walls].Add(item); other = false; } // 墙壁
                         if (item.createTile != -1 && Main.tileFrameImportant[item.createTile])
-                        { categories["Placeables"]["Furniture"].Add(item); other = false; } // 家具
+                        { categories[placeables][furniture].Add(item); other = false; } // 家具
                         if (ItemID.Sets.IsFishingCrate[item.type])
-                        { categories["Placeables"]["FishingCrate"].Add(item); other = false; } // 宝匣
+                        { categories[placeables][fishingCrate].Add(item); other = false; } // 宝匣
                         if (item.createTile != -1 && item.expert)
-                        { categories["Placeables"]["Relic"].Add(item); other = false; } // 圣物
+                        { categories[placeables][relic].Add(item); other = false; } // 圣物
 
                         // 消耗品
                         if (item.ammo != 0)
-                        { categories["Consumables"]["Ammo"].Add(item); other = false; } // 弹药
+                        { categories[consumables][ammo].Add(item); other = false; } // 弹药
                         if (item.UseSound == SoundID.Item3 && !ItemID.Sets.IsFood[item.type])
-                        { categories["Consumables"]["Potions"].Add(item); other = false; } // 药水
+                        { categories[consumables][potions].Add(item); other = false; } // 药水
                         if (ItemID.Sets.IsFood[item.type])
-                        { categories["Consumables"]["Food"].Add(item); other = false; } // 食物
+                        { categories[consumables][food].Add(item); other = false; } // 食物
                         if (sortingPriorityBossSpawns[item.type] != -1 && !sortingPriorityBossSpawnsExclusions.Contains(item.type) || item.type == ItemID.PirateMap || item.type == ItemID.SnowGlobe || item.type == ItemID.DD2ElderCrystal)
-                        { categories["Consumables"]["BossSummons"].Add(item); other = false; } // Boss召唤物
+                        { categories[consumables][bossSummons].Add(item); other = false; } // Boss召唤物
                         if (ItemID.Sets.BossBag[item.type] && item.expert)
-                        { categories["Consumables"]["BossBag"].Add(item); other = false; } // 宝藏袋
+                        { categories[consumables][bossBag].Add(item); other = false; } // 宝藏袋
                         if (item.bait > 0)
-                        { categories["Consumables"]["Bait"].Add(item); other = false; } // 鱼饵
+                        { categories[consumables][bait].Add(item); other = false; } // 鱼饵
                         if (item.questItem)
-                        { categories["Consumables"]["QuestFish"].Add(item); other = false; } // 任务鱼
+                        { categories[consumables][questFish].Add(item); other = false; } // 任务鱼
                         if (ItemID.Sets.IsAPickup[item.type])
-                        { categories["Consumables"]["Pickup"].Add(item); other = false; } // 拾取物
+                        { categories[consumables][pickup].Add(item); other = false; } // 拾取物
 
                         // 其它
                         if (item.material)
-                        { categories["Other"]["Materials"].Add(item); other = false; } // 材料
+                        { categories[others][materials].Add(item); other = false; } // 材料
                         if (ItemID.Sets.IsAKite[item.type])
-                        { categories["Other"]["Kites"].Add(item); other = false; } // 风筝
+                        { categories[others][kites].Add(item); other = false; } // 风筝
                         if (item.fishingPole > 0)
-                        { categories["Other"]["Poles"].Add(item); other = false; } // 鱼竿
+                        { categories[others][poles].Add(item); other = false; } // 鱼竿
                         if (item.paint > 0 || ItemID.Sets.IsPaintScraper[item.type])
-                        { categories["Other"]["Paint"].Add(item); other = false; } // 油漆
+                        { categories[others][paint].Add(item); other = false; } // 油漆
                         if (item.dye != 0)
-                        { categories["Other"]["Dyes"].Add(item); other = false; } // 染料
+                        { categories[others][dyes].Add(item); other = false; } // 染料
                         if (other)
-                        { categories["Other"]["Other"].Add(item); } // 其他
+                        { categories[others][oothers].Add(item); } // 其他
                     }
                 }
 
@@ -140,80 +151,96 @@ namespace Terraria.Chat
                 {
                     foreach (var subCategory in category.Value)
                     {
-                        if (category.Key == "Weapons")
+                        if (category.Key == weapons)
                         {
                             subCategory.Value.Sort((x, y) => x.damage.CompareTo(y.damage));
                         }
-                        else if (category.Key == "Tools")
+                        else if (category.Key == tools)
                         {
-                            if (subCategory.Key == "Pickaxes")
+                            if (subCategory.Key == pickaxes)
                             {
                                 subCategory.Value.Sort((x, y) => x.pick.CompareTo(y.pick));
                             }
-                            else if (subCategory.Key == "Axes")
+                            else if (subCategory.Key == axes)
                             {
                                 subCategory.Value.Sort((x, y) => x.axe.CompareTo(y.axe));
                             }
-                            else if (subCategory.Key == "Hammers")
+                            else if (subCategory.Key == hammers)
                             {
                                 subCategory.Value.Sort((x, y) => x.hammer.CompareTo(y.hammer));
                             }
-                            else if (subCategory.Key == "Drill")
+                            else if (subCategory.Key == drill)
                             {
                                 subCategory.Value.Sort((x, y) => x.pick.CompareTo(y.pick));
                             }
-                            else if (subCategory.Key == "Chainsaw")
+                            else if (subCategory.Key == chainsaw)
                             {
                                 subCategory.Value.Sort((x, y) => x.axe.CompareTo(y.axe));
                             }
                         }
-                        else if (category.Key == "Armor")
+                        else if (category.Key == armor)
                         {
                             subCategory.Value.Sort((x, y) => x.defense.CompareTo(y.defense));
                         }
-                        else if (category.Key == "Accessories")
+                        else if (category.Key == accessories)
                         {
-                            if (subCategory.Key == "Wings")
+                            if (subCategory.Key == wings)
                             {
                                 subCategory.Value.Sort((x, y) => x.wingSlot.CompareTo(y.wingSlot));
                             }
                         }
-                        else if (category.Key == "Placeables")
+                        else if (category.Key == placeables)
                         {
 
                         }
-                        else if (category.Key == "Consumables")
+                        else if (category.Key == consumables)
                         {
-                            if (subCategory.Key == "Ammo")
+                            if (subCategory.Key == ammo)
                             {
                                 subCategory.Value.Sort((x, y) => x.ammo.CompareTo(y.ammo));
                             }
-                            else if (subCategory.Key == "Food")
+                            else if (subCategory.Key == food)
                             {
                                 subCategory.Value.Sort((x, y) => ItemID.Sets.IsFood[x.type].CompareTo(ItemID.Sets.IsFood[y.type]));
                             }
-                            else if (subCategory.Key == "BossSummons")
+                            else if (subCategory.Key == bossSummons)
                             {
                                 subCategory.Value.Sort((x, y) => sortingPriorityBossSpawns[x.type].CompareTo(sortingPriorityBossSpawns[y.type]));
                             }
-                            else if (subCategory.Key == "BossBag")
+                            else if (subCategory.Key == bossBag)
                             {
                                 subCategory.Value.Sort((x, y) => ItemID.Sets.BossBag[x.type].CompareTo(ItemID.Sets.BossBag[y.type]));
                             }
-                            else if (subCategory.Key == "Bait")
+                            else if (subCategory.Key == bait)
                             {
                                 subCategory.Value.Sort((x, y) => x.bait.CompareTo(y.bait));
                             }
                         }
-                        else if (category.Key == "Other")
+                        else if (category.Key == others)
                         {
-                            if (subCategory.Key == "Poles")
+                            if (subCategory.Key == poles)
                             {
                                 subCategory.Value.Sort((x, y) => x.fishingPole.CompareTo(y.fishingPole));
                             }
                         }
                     }
                 }
+
+                // 写入
+                var writer = new StreamWriter("categories.lua");
+                writer.WriteLine("categories = {");
+                foreach (var category in categories)
+                {
+                    writer.WriteLine("\t" + category.Key + " = {");
+                    foreach (var subCategory in category.Value)
+                    {
+                        writer.WriteLine("\t\t" + subCategory + " = {" +
+                            string.Join(", ", subCategory.Value.Select(i => i.type.ToString())) +
+                            "}");
+                    }
+                }
+                writer.WriteLine("}");
+                writer.Close();
             }
             // ###################################################################################
             IChatCommand chatCommand;
